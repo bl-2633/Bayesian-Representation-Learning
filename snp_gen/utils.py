e4f5e2207f7192dcdfcb7fd925892ffe3bb2a90c
bl-2633 <bl2633@columbia.edu>
2017-11-26 05:55:39 +0000

    modifications to models

--- snp_gen/utils.py
+++ snp_gen/utils.py
@@ -8,8 +8,7 @@ import numpy as np
 
 def generator(data, batch_size):
 
-    """Generate batches, one with respect to each array's first axis."""
-    starts = 0  # pointers to where we are in iteration
+    starts = 0
     while True:
         stop = starts + batch_size
         diff = stop - data.shape[0]
@@ -19,6 +18,25 @@ def generator(data, batch_size):
         else:
             batch = np.concatenate((data[starts:], data[:diff]))
             starts = diff
-        batch = batch.astype(np.float32) / 255.0  # normalize pixel intensities
-        batch = np.random.binomial(1, batch)  # binarize images
+        #batch = batch.astype(np.float32) / 255.0
+        #batch = np.random.binomial(1, batch)
         yield batch
+
+
+def generator_xy(arrays, batch_size):
+    starts = [0] * len(arrays)  # pointers to where we are in iteration
+    while True:
+        batches = []
+        for i, array in enumerate(arrays):
+            start = starts[i]
+            stop = start + batch_size
+            diff = stop - array.shape[0]
+            if diff <= 0:
+                batch = array[start:stop]
+                starts[i] += batch_size
+            else:
+                batch = np.concatenate((array[start:], array[:diff]))
+                starts[i] = diff
+            batches.append(batch)
+        yield batches
+
